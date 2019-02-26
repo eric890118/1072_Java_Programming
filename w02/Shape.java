5e6f57bbb4d9c57ef6581a1686b2d8942ba4ae70
eric890118 <43335682+eric890118@users.noreply.github.com>
2019-02-26 17:52:35 +0800

    Create Shape.java

---
+++ w02/Shape.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+public class Shape {
+	protected int height;
+
+	public Shape(int h) {
+		height = h;
+	}
+
+	public Shape() {
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+}
